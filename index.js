d74e3f3d1b82c7c6fcad2d9f06527fa04485ed58
Stella Mazzillo <stellagress5@gmail.com>
2023-04-06 00:20:43 -0400

    complete moving things with js by acting on events

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+
+
+// document.addEventListener("keydown", function (event) {
+//     if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
+  
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   });
+
+const dodger = document.getElementById('dodger')
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px" , "")
+    console.log(leftNumbers)
+
+    const left = parseInt(leftNumbers, 10)
+    console.log(left)
+
+
+    if(left > 0){
+        dodger.style.left = `${left -1}px`
+    }
+}
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+
+      moveDodgerLeft()
+    }
+  });
+
+
+
+
+
+
+  function moveDodgerRight(){
+
+    const rightNumbers = dodger.style.left.replace("px" , "")
+    console.log(rightNumbers)
+
+    
+
+    const right = parseInt(rightNumbers, 10)
+    console.log(right)
+
+
+    //if(right > 0){
+      if(right < 360){  
+        dodger.style.left = `${right + 1}px`
+    }
+}
+
+
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+
+      moveDodgerRight()
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
